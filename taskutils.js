64019a4d5c61acb21fadececbe7b310157fc3114
Andrey Pissantchev <andrey@duckduckgo.com>
2015-08-10 09:40:53 -0400

    improved algorithm for batchifying with multiple browsers

--- taskutils.js
+++ taskutils.js
@@ -1,6 +1,41 @@
 var MIN_TASKS_PER_BATCH = 2;
 
 /**
+ * Split an array of tasks based on maximum number of batches for this list of tasks
+ *
+ * @param {array} tasks
+ * @param {integer} maxBatches
+ * @returns {array} batches (array of arrays)
+ */
+function splitIntoBatches(tasks, maxBatches) {
+    var currentBatch,
+        batches = [],
+        tasksPerBatch = Math.max(
+            MIN_TASKS_PER_BATCH,
+            Math.ceil(tasks.length / maxBatches)
+        );
+
+    // create clone of tasks array so we don't edit it
+    tasks = tasks.concat();
+
+    for (var i = 0; i < maxBatches; i++) {
+        if (!tasks.length) {
+            break;
+        }
+
+        currentBatch = [];
+
+        while (currentBatch.length < tasksPerBatch && tasks.length) {
+            currentBatch.push(tasks.shift());
+        }
+
+        batches.push(currentBatch);
+    }
+
+    return batches;
+}
+
+/**
  * Split an array of tasks into batches that can be run
  * simultaneously
  *
@@ -9,16 +44,13 @@ var MIN_TASKS_PER_BATCH = 2;
  * @returns {array} batches (array of arrays)
  */
 exports.batchify = function (tasks, maxParallelTasks) {
-    var batches = [],
+    var batches,
         browsers = [],
-        tasksPerBrowser = {},
-        currentBatch,
-        tasksLeft = tasks.concat(),
-        i;
+        tasksPerBrowser = {};
 
     // first, collect all the browsers we've got
     // and group the tasks by browser
-    for (i = 0; i < tasks.length; i++) {
+    for (var i = 0; i < tasks.length; i++) {
         if (browsers.indexOf(tasks[i].browser) === -1) {
             browsers.push(tasks[i].browser);
 
@@ -30,30 +62,20 @@ exports.batchify = function (tasks, maxParallelTasks) {
 
     // if we've got more than one browser, try to do one browser per batch
     if (browsers.length > 1 && browsers.length <= maxParallelTasks) {
+        batches = [];
+
+        var maxBatchesPerBrowser = Math.floor(maxParallelTasks / browsers.length),
+            tasksLeft;
+
         for (i = 0; i < browsers.length; i++) {
-            batches.push(tasksPerBrowser[browsers[i]]);
+            tasksLeft = tasksPerBrowser[browsers[i]];
+
+            batches = batches.concat(splitIntoBatches(tasksLeft, maxBatchesPerBrowser));
         }
 
     // else split the tasks evenly amongst all the batches we've got available
     } else {
-        var tasksPerBatch = Math.max(
-            MIN_TASKS_PER_BATCH,
-            Math.ceil(tasks.length / maxParallelTasks)
-        );
-
-        for (i = 0; i < maxParallelTasks; i++) {
-            if (!tasksLeft.length) {
-                break;
-            }
-
-            currentBatch = [];
-
-            while (currentBatch.length < tasksPerBatch && tasksLeft.length) {
-                currentBatch.push(tasksLeft.shift());
-            }
-
-            batches.push(currentBatch);
-        }
+        batches = splitIntoBatches(tasks, maxParallelTasks);
     }
 
     return batches;
